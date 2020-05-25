e2eef82fdeefd1c045cb985892da82927be28987
limeunchong <ui706@naver.com>
2020-05-25 21:12:11 +0900

    Add Customer Delete

--- server.js
+++ server.js
@@ -25,13 +25,16 @@ const multer = require('multer')
 const upload = multer({ dest: './upload' })
 
 app.get('/api/customers', (req, res) => {
-  connection.query('select * from CUSTOMER', (err, rows, fields) => {
-    res.send(rows)
-  })
+  connection.query(
+    'select * from CUSTOMER WHERE isDeleted = 0',
+    (err, rows, fields) => {
+      res.send(rows)
+    },
+  )
 })
 app.use('/image', express.static('./upload'))
 app.post('/api/customers', upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER VALUES (NULL,?,?,?,?,?)'
+  let sql = 'INSERT INTO CUSTOMER VALUES (NULL,?,?,?,?,?,now(), 0)'
   let image = '/image/' + req.file.filename
   let name = req.body.userName
   let birthday = req.body.birthday
@@ -39,6 +42,14 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
   let job = req.body.job
   let params = [image, name, birthday, gender, job]
   console.log([image, name, birthday, gender, job])
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows)
+  })
+})
+
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 where id = ?'
+  let params = [req.params.id]
   connection.query(sql, params, (err, rows, fields) => {
     res.send(rows)
   })
